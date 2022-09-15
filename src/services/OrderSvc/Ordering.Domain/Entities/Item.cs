560256c7b9ac19bd410d8ab31ba4668c62c4a861
Chris <eng.christhuku@gmail.com>
2022-09-15 20:54:45 +0300

    feat ordering domain

---
+++ src/services/OrderSvc/Ordering.Domain/Entities/Item.cs
@@ -0,0 +1,26 @@
+﻿using Common.Base.Shared;
+using Common.Base.Shared.ValueObjects;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ordering.Domain.Entities
+{
+  [Table("Items")]
+  public class Item : EntityBase
+  {
+    public static Item Create(Guid orderId, string description, string name, Money amount) =>
+      new Item(orderId, description, name, amount);
+    private Item(Guid orderId, string description, string name, Money amount)
+    {
+      OrderId = orderId;
+      Description = description ?? throw new ArgumentNullException(nameof(description));
+      Name = name ?? throw new ArgumentNullException(nameof(name));
+      Amount = amount ?? throw new ArgumentNullException(nameof(amount));
+    }
+
+    public Guid OrderId { get; set; }
+    public string Description { get; set; }
+    public string Name { get; set; }
+    public Money Amount { get; set; }
+    public Order? Order { get; set; }
+  }
+}
---
+++ src/services/OrderSvc/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using Common.Base.Shared;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ordering.Domain.Entities
+{
+  [Table("Orders")]
+  public  class Order : EntityBase
+  {
+    public static Order Create(Guid businessId, string orderDescription, Guid user, List<Item> items)
+      => new Order(businessId, orderDescription, user, items);
+    private Order(Guid businessId, string orderDescription, Guid user,  List<Item> items)
+    {
+      BusinessId = businessId;
+      OrderDescription = orderDescription ?? throw new ArgumentNullException(nameof(orderDescription));
+      User = user;
+      GovAPIStatus = 0;
+      Items = items ?? throw new ArgumentNullException(nameof(items));
+    }
+
+    public Guid BusinessId { get; set; }
+    public string OrderDescription { get; set; }
+    public Guid User { get; set; }
+    /// <summary>
+    /// 200/201 was logged and acknowledged
+    /// Others should be retried or so
+    /// </summary>
+    public  int  GovAPIStatus { get; set; }
+    public List<Item> Items { get; set; }
+  }
+}
