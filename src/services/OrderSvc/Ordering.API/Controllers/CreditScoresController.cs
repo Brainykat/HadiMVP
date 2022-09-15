0d0af0b91cd15f331ab1c23f088ed49845a24f1b
Chris <eng.christhuku@gmail.com>
2022-09-15 22:40:24 +0300

    feat ordering controllers

---
+++ src/services/OrderSvc/Ordering.API/Controllers/CreditScoresController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Services.Interfaces;
+
+namespace Ordering.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CreditScoresController : ControllerBase
+  {
+    private readonly ICreditScoreServices service;
+    public CreditScoresController(ICreditScoreServices service)
+    {
+      this.service = service ?? throw new ArgumentNullException(nameof(service));
+    }
+
+    /// <summary>
+    /// UNDONE: We should retrieve this business ID from claims in Token but lets pass it for now
+    /// </summary>
+    /// <param name="businessId"></param>
+    /// <returns></returns>
+    [HttpGet("{businessId}")]
+    public async Task<IActionResult> Get(Guid businessId)
+      => Ok(await service.GetCreditScore(businessId));
+  }
+}
---
+++ src/services/OrderSvc/Ordering.API/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Services.Interfaces;
+using OrderingDtos;
+
+namespace Ordering.API.Controllers
+{
+  [Route("api/[controller]/[action]")]
+  [ApiController]
+  public class OrdersController : ControllerBase
+  {
+    private readonly IOrderService service;
+    public OrdersController(IOrderService service)
+    {
+      this.service = service ?? throw new ArgumentNullException(nameof(service));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllOrders()
+      => Ok(await service.GetAllOrders());
+
+    [HttpGet("{businessId}")]
+    public async Task<IActionResult> GetOrdersPerBusines(Guid businessId) 
+      => Ok(await service.GetOrdersPerBusines(businessId));
+
+    [HttpGet("{orderId}")]
+    public async Task<IActionResult> GetOrder(Guid orderId)
+      => Ok(await service.Get(orderId));
+
+    //TODO get user from claims
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] OrderDto dto)
+    => await service.Add(dto, Guid.Empty);
+
+
+    [HttpPut("{orderId}")]
+    public async Task<IActionResult> Update(Guid orderId, [FromBody] OrderDto dto)
+    => await service.Update(orderId, dto);
+
+
+    [HttpDelete("{orderId}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(Guid orderId)
+    => await service.Delete(orderId);
+  }
+}
