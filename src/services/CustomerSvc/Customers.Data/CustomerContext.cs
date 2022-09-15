798be56911908b75e60a9406ec42b725c5540771
Chris <eng.christhuku@gmail.com>
2022-09-15 15:23:48 +0300

    feat customer context

---
+++ src/services/CustomerSvc/Customers.Data/CustomerContext.cs
@@ -0,0 +1,26 @@
+﻿using Customers.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers.Data
+{
+  public class CustomerContext : DbContext
+  {
+    public CustomerContext(DbContextOptions<CustomerContext> options)
+           : base(options) { }
+    public DbSet<BusinessLocation> BusinessLocations { get; set; }
+    public DbSet<CustomerBusiness> CustomerBusinesses { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<EmployeeDepartment> EmployeeDepartments { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfiguration(new CustomerBusinessConfig());
+    }
+  }
+}
---
+++ src/services/CustomerSvc/Customers.Data/DBConfigurations/CustomerBusinessConfig.cs
@@ -0,0 +1,31 @@
+﻿using Customers.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Customers.Data.DBConfigurations
+{
+  public class CustomerBusinessConfig : IEntityTypeConfiguration<CustomerBusiness>
+  {
+    public void Configure(EntityTypeBuilder<CustomerBusiness> builder)
+    {
+      builder.Property(e => e.Name)
+          .IsRequired()
+          .HasMaxLength(80);
+      builder.Property(e => e.Phone)
+          .IsRequired()
+          .HasMaxLength(15);
+      builder.Property(e => e.Email)
+          .IsRequired()
+          .HasMaxLength(50);
+      builder.Property(e => e.RegistrationNumber)
+          .IsRequired()
+          .HasMaxLength(80);
+      builder.OwnsOne(i => i.Owner, f =>
+      {
+        f.Property(n => n.First).IsRequired().HasMaxLength(50);
+        f.Property(n => n.Sur).IsRequired().HasMaxLength(50);
+        f.Property(n => n.Middle).HasMaxLength(50);
+      });
+    }
+  }
+}
