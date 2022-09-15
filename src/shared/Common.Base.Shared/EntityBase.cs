f9dce135226f2793ee5d8e3ecb307094a9a23f8b
Chris <eng.christhuku@gmail.com>
2022-09-15 14:34:14 +0300

    feat entity base

--- src/shared/Common.Base.Shared/EntityBase.cs
+++ src/shared/Common.Base.Shared/EntityBase.cs
@@ -1,7 +1,63 @@
-﻿namespace Common.Base.Shared
+﻿using StackifyLib.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace Common.Base.Shared
 {
   public class EntityBase
   {
+    [Key]
+    public virtual Guid Id { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    /// <summary>
+    /// 0 = New,
+    /// 1 = Authorized,
+    /// 9 = Deleted
+    /// </summary>
+    public int Status { get; set; } = 0;
+    public bool IsTransient() => Id == Guid.Empty;
 
+    /// <summary>
+    /// See https://github.com/stackify/stackify-api-dotnet/blob/master/Src/StackifyLib/Utils/SequentialGuid.cs
+    /// </summary>
+    /// <param name="type"></param>
+    public void GenerateNewIdentity()
+    {
+      if (IsTransient())
+      {
+        Id = SequentialGuid.NewGuid();
+      }
+    }
+
+    public void ChangeCurrentIdentity(Guid id)
+    {
+      if (id != Guid.Empty)
+        Id = id;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (!(obj is EntityBase other))
+        return false;
+      if (ReferenceEquals(this, other))
+        return true;
+      if (GetType() != other.GetType())
+        return false;
+      if (other.IsTransient() || IsTransient())
+        return false;
+      return Id == other.Id;
+    }
+
+    public static bool operator ==(EntityBase left, EntityBase right)
+    {
+      if (left is null && right is null)
+        return true;
+      if (left is null || right is null)
+        return false;
+      return left.Equals(right);
+    }
+
+    public static bool operator !=(EntityBase left, EntityBase right) => !(left == right);
+
+    public override int GetHashCode() => (GetType().ToString() + Id).GetHashCode();
   }
 }
